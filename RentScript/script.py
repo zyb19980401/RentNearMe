02a29ca0ca4ef086d3cbfce808124e10db5f698b
chriszhang <chris19980401@gmail.com>
2019-10-17 03:54:20 -0400

    with a working cript and a price filter

--- RentScript/script.py
+++ RentScript/script.py
@@ -1,57 +1,13 @@
 from bs4 import BeautifulSoup
-import requests
 from requests import get
 import re
-headers = ({'User-Agent':
-            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+import csv
+import pandas as pd
 
-condoforsale = "https://condos.ca/toronto/condos-for-sale"
-response = get(condoforsale, headers=headers)
-soup = BeautifulSoup(response.text, 'lxml')
+csv_file = open('condoData.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['price', 'address', 'bedRoomInfo', 'bathRoomInfo', 'parkingInfo', 'sizeInfo', 'link', 'fee'])
 
-
-
-
-#try to find all the url and store them in to a list
-next_pages = soup.find('ul', class_="_3h0Z4")
-next_page_links = next_pages.findAll('a', class_="_3uJE2")
-allUrlInThePage = [link['href'] for link in next_page_links]
-print(allUrlInThePage)
-# for link in next_page_links:
-#     a = link['href']
-#     print(a)
-    # address = str(unit.a['alt'])
-    # temp = link.a['href']
-    # print(temp)
-
-
-
-
-# print (next_page.a["href"])
-# print(next_page_link)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-house_containers = soup.find_all('div', class_="FrmWb")
-info_list = []
 def getHouseInfo(houseTypeHtml):
     # a function that takes a house html and return a list that contains[bedroom Info, bathroom Info,Parking \\
     # Info, size Info]
@@ -67,50 +23,94 @@ def getHouseInfo(houseTypeHtml):
     return resultInfo
 
 
+def getCondo(souptoscrap):
+    house_containers = souptoscrap.find_all('div', class_="FrmWb")
+    allHouseInThePage = []
+    for unit in house_containers:
+        #find each unit from the house_containers, we will scrap information from each unit block
+        # print(unit)
+        link = 'https://condos.ca' + str(unit.a['href'])
+        address = str(unit.a['alt'])
+        price = unit.find('div', class_="_2PKdn").text[1:]
+        price = price.replace(",", "")
+        if unit.find('div', class_= "YjyI8").text:
+            fee = unit.find('div', class_= "YjyI8").text
+        else:
+            fee = "0"
+        houseType = str(unit.find('div', class_="_3FIJA"))
+        houseInfo = getHouseInfo(houseType)  # return a list in form of [bedroom Info, bathroom Info, size Info]
+        if len(houseInfo) == 4:
+            bedRoomInfo = houseInfo[0]
+            bathRoomInfo = houseInfo[1]
+            parkingInfo = houseInfo[2]
+            sizeInfo = houseInfo[3]
+        else:
+            continue
+        result = [price, address, bedRoomInfo, bathRoomInfo, parkingInfo, sizeInfo, link, fee]
+        allHouseInThePage.append(result)
+    return allHouseInThePage
+
+
+def loadData(IsRent):
+    headers = ({'User-Agent':
+                    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+    condoForSale = "https://condos.ca/toronto/condos-for-sale"
+    condoForRent = "https://condos.ca/toronto/condos-for-rent"
+    if(IsRent):
+        nextPage = condoForRent
+    else:
+        nextPage = condoForSale
+    while nextPage is not None:
+        response = get(nextPage, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        # print(getCondo(soup))
+        acc = getCondo(soup)
+        for condo in acc:
+            csv_writer.writerow(condo)
+        nextPagesBar = soup.find('ul', class_="_3h0Z4")
+        allLiInNextPages = nextPagesBar.findAll('li')
+        for i in range(len(allLiInNextPages)):
+            try:
+                if i == len(allLiInNextPages) - 1:
+                    print("we have reach the last page")
+                    nextPage = None
+                    break
+                elif len(allLiInNextPages[i]['class']) > 1:
+                    # print(allLiInNextPages[i])
+                    # print(allLiInNextPages[i]['class'])
+                    urlWithoutHttp = allLiInNextPages[i + 1].find('a', class_="_3uJE2")['href']
+                    new_url = "https://condos.ca" + urlWithoutHttp
+                    nextPage = new_url
+
+                    print(new_url)
+
+                    break
+            except AttributeError:
+                print("an exception ")
 
 
 
-for unit in house_containers:
-    #find each unit from the house_containers, we will scrap information from each unit block
-    # print(unit)
-    link = 'https://condos.ca' + str(unit.a['href'])
-    address = str(unit.a['alt'])
-    price = unit.find('div', class_="_2PKdn")
-    houseType = str(unit.find('div', class_="_3FIJA"))
-    houseInfo = getHouseInfo(houseType)  # return a list in form of [bedroom Info, bathroom Info, size Info]
-    # print(houseInfo)
-    bedRoomInfo = houseInfo[0]
-    bathRoomInfo = houseInfo[1]
-    parkingInfo = houseInfo[2]
-    sizeInfo = houseInfo[3]
+if __name__ == "__main__":
+    # execute only if run as a script
+    print("Hello World")
+    Option = input("Do u want to rent or buy?: ")
+    if Option == "buy":
+        print("Plz hold while we forming the csv file for ur data")
+        loadData(False)
+    else:
+        print("Plz hold while we forming the csv file for ur data")
+        loadData(True)
+    rawdata = pd.read_csv("./condoData.csv")
+    UpperLimit = int(input("What is ur upper Limit on Price?: "))
+    LowerLimit = int(input("What is ur lower Limit on Price?: "))
 
+    csv_file = open('condoDataPrice.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['price', 'address', 'bedRoomInfo', 'bathRoomInfo', 'parkingInfo', 'sizeInfo', 'link', 'fee'])
 
-
-    # print()
-    # print(address)
-    # print(link)
-    # print(unit)
-a = house_containers
-
-
-
-
-
-# print(a)
-
-# print(soup)
-# print(response.text[:1000])
-#
-# https://condos.ca/toronto/quartz-spectra-75-queens-wharf-rd-85-queens-wharf-rd/unit-3908-C4600032
-#
-# https://condos.ca/toronto/43-eglinton-avenue-east-condos-43-eglinton-ave-e/unit-308-C4599686
-
-
-# tessst = "https://condos.ca/toronto/the-republic-south-tower-70-roehampton-ave/unit-221-C4600019"
-# response = get(tessst, headers=headers)
-# soupa = BeautifulSoup(response.text, 'lxml')
-# #
-# image_scr = [x['src'] for x in soupa.findAll('img')]
-# css_link = [x['href'] for x in soupa.findAll('link')]
-# print(image_scr)
-# print(css_link)
+    for i in range(rawdata.shape[0]):
+        temp = rawdata.iloc[i]
+        price = temp["price"]
+        if LowerLimit <= int(price) <= UpperLimit:
+            acc = rawdata.iloc[i].values.tolist()
+            csv_writer.writerow(acc)
