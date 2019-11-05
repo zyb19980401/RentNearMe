618b532243975228b5ae9e3b5e0094c104074806
chriszhang <chris19980401@gmail.com>
2019-11-05 18:08:46 -0500

    housinglinker is changed to use python sql

--- ElectronJs/gui/linkers/HousingLinker.js
+++ ElectronJs/gui/linkers/HousingLinker.js
@@ -6,19 +6,21 @@ let options = {}
 
 
 function getData() {
+    buyOrRent = document.getElementById("buyOrRentInput").value
     upperLimit = document.getElementById("upperLimit").value
     lowerLImit = document.getElementById("lowerLimit").value
     addressInput = document.getElementById("addressInput").value
-    lowerLImit = document.getElementById("lowerLimit").value
     bedroomInput = document.getElementById("bedroomInput").value
     washroomInput = document.getElementById("washroomInput").value
     parkingInput = document.getElementById("parkingInput").value
-    finalHtmlInput =[upperLimit,lowerLImit,addressInput,lowerLImit,bedroomInput,washroomInput,parkingInput]
+    finalHtmlInput =[buyOrRent,upperLimit,lowerLImit,addressInput,bedroomInput,washroomInput,parkingInput]
+    console.log(finalHtmlInput)
     options = {
         scriptPath: path.join(__dirname, '/../engine/'),
         //have to add a bracket, otherwise it will be 7 arguments
         args: [finalHtmlInput]
     }
+    console.log("i am in get data")
 
     let pyshell = new PythonShell('script.py', options);
 
